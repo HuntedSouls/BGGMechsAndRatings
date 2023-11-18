49813ba1d1ee4f5f69d8d29fe388418a8cb4e8dc
Joshua Kritz <joshuakritz2@gmail.com>
2023-11-18 15:18:39 +0000

    Reformated data v1 and starting analysis

--- BGGData/DataAnalysis.py
+++ BGGData/DataAnalysis.py
@@ -1,3 +1,55 @@
 import pandas as pd
-def CountNonRanked(dataFrame):
-    dataFrame
+import matplotlib
+import matplotlib.pyplot as plt
+def FilterData(dataFrame):
+    #remove all subtyperanks nan so we can filer by them
+    dataFrame = dataFrame[dataFrame['subtype_rank'].notna()]
+    rankedDF = dataFrame.query("subtype_rank != 'Not Ranked'")
+
+    #Fill nan in average so we can filter 0s
+    rankedDF["bayesaverage"] =rankedDF["bayesaverage"].fillna(0)
+    rankedDF = rankedDF.query("bayesaverage != 0.0")
+
+    implList = rankedDF.groupby("gameID")["implementation"].apply(list).reset_index(name='impls')
+
+    # filter through implementations
+    for gameID in rankedDF["gameID"]:
+        implementations = implList.loc[implList['gameID'] == gameID]
+        cleaned = [x for x in implementations if str(x) != 'nan']
+        listOfGameIDS =[]
+        listOfGameIDS.append(gameID)
+        for name in cleaned:
+            foundGames = rankedDF.loc[rankedDF['name'] == name]
+            if not foundGames.empty:
+                listOfGameIDS.append(foundGames.iloc[0]["gameID"])
+
+
+def HandleMechanicsMess(rankedDF, listOfGameIDS):
+    mechList = rankedDF.groupby("gameID")["mechanic"].apply(list).reset_index(name='mechs')
+    thisMechs ={}
+    for gameID in listOfGameIDS:
+        mechanics = mechList.loc[mechList['gameID'] == gameID]
+        cleaned = [x for x in mechanics if str(x) != 'nan']
+        thisMechs[gameID] = cleaned
+
+    equal=[]
+    for gameID in listOfGameIDS:
+        if gameID == listOfGameIDS[0]:
+            continue
+        if thisMechs[gameID].sort() == thisMechs[listOfGameIDS[0]].sort():
+            equal.append(gameID)
+    #do some magic if they found equal games
+    if len(equal)>0:
+        avgRatingList =[]
+        baeysianRatingList =[]
+        stdRatingList =[]
+        voteCountList=[]
+        yearList=[]
+        for gameID in equal:
+            selected = rankedDF.loc[rankedDF['gameID'] == gameID]
+            avgRatingList.append(selected.iloc[0]["average"])
+            baeysianRatingList.append(selected.iloc[0]["bayesaverage"])
+            stdRatingList.append(selected.iloc[0]["stddev"])
+            voteCountList.append(selected.iloc[0]["usersrated"])
+            yearList.append(selected.iloc[0]["yearpublished"])
+        #define the higher year to be the entry and do some math see combine stdeviation
--- BGGData/main.py
+++ BGGData/main.py
@@ -1,8 +1,10 @@
 # This is a sample Python script.
 import APIGet
+import DataAnalysis
+import reformatData
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import pandas as pd
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,7 +13,11 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    APIGet.AggregateCSV("RawData")
+    dataFrame = pd.read_csv("RawData/Aggregated.csv")
+    #DataAnalysis.CountNonRanked(dataFrame)
+    reformated = reformatData.addMechanics(dataFrame)
+    print(reformated.head(10))
+    reformated.to_csv("RawData/ReformatedData.csv")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

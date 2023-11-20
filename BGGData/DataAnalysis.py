d75a7c701927f1ffe463c45cd842e00e8de22d70
Joshua Kritz <joshuakritz2@gmail.com>
2023-11-20 12:24:07 +0000

    minor fixes

--- BGGData/DataAnalysis.py
+++ BGGData/DataAnalysis.py
@@ -1,30 +1,42 @@
+import math
+
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 def FilterData(dataFrame):
     #remove all subtyperanks nan so we can filer by them
-    dataFrame = dataFrame[dataFrame['subtype_rank'].notna()]
+    #filterDF = dataFrame[dataFrame['subtype_rank'].notna()]
     rankedDF = dataFrame.query("subtype_rank != 'Not Ranked'")
 
     #Fill nan in average so we can filter 0s
     rankedDF["bayesaverage"] =rankedDF["bayesaverage"].fillna(0)
     rankedDF = rankedDF.query("bayesaverage != 0.0")
 
-    implList = rankedDF.groupby("gameID")["implementation"].apply(list).reset_index(name='impls')
+    blacklistedGameIDs=rankedDF["gameID"].unique()
+    filteredDF = dataFrame
+    for gameID in blacklistedGameIDs:
+        filteredDF = filteredDF.loc[filteredDF['gameID'] != gameID]
+
+    implList = filteredDF.groupby("gameID")["implementation"].apply(list).reset_index(name='impls')
 
     # filter through implementations
+    solvedGameID=[]
     for gameID in rankedDF["gameID"]:
         implementations = implList.loc[implList['gameID'] == gameID]
         cleaned = [x for x in implementations if str(x) != 'nan']
         listOfGameIDS =[]
         listOfGameIDS.append(gameID)
+        solvedGameID.append(gameID)
         for name in cleaned:
             foundGames = rankedDF.loc[rankedDF['name'] == name]
             if not foundGames.empty:
-                listOfGameIDS.append(foundGames.iloc[0]["gameID"])
+                newID =foundGames.iloc[0]["gameID"]
+                listOfGameIDS.append(newID)
+                solvedGameID.append(newID)
+        rankedDF = HandleReimplementations(rankedDF,listOfGameIDS)
+        print("finished game id: ",gameID)
 
-
-def HandleMechanicsMess(rankedDF, listOfGameIDS):
+def HandleReimplementations(rankedDF, listOfGameIDS):
     mechList = rankedDF.groupby("gameID")["mechanic"].apply(list).reset_index(name='mechs')
     thisMechs ={}
     for gameID in listOfGameIDS:
@@ -39,6 +51,7 @@ def HandleMechanicsMess(rankedDF, listOfGameIDS):
         if thisMechs[gameID].sort() == thisMechs[listOfGameIDS[0]].sort():
             equal.append(gameID)
     #do some magic if they found equal games
+    mainGame = pd.Series()
     if len(equal)>0:
         avgRatingList =[]
         baeysianRatingList =[]
@@ -52,4 +65,27 @@ def HandleMechanicsMess(rankedDF, listOfGameIDS):
             stdRatingList.append(selected.iloc[0]["stddev"])
             voteCountList.append(selected.iloc[0]["usersrated"])
             yearList.append(selected.iloc[0]["yearpublished"])
-        #define the higher year to be the entry and do some math see combine stdeviation
+        #define the older year to be the entry and delete all "tied" games
+        olderYear = min(yearList)
+        olderGameID = equal[yearList.index(olderYear)]
+        mainGame = rankedDF.iloc[0][rankedDF['yearpublished'] == olderGameID]
+        for gameID in equal:
+            rankedDF = rankedDF[rankedDF["gameID"] != gameID]
+        #and do some math see combine averages and stdeviation
+        mainGame["average"] = weightedAverage(voteCountList,avgRatingList)
+        mainGame["bayesaverage"] = weightedAverage(voteCountList, baeysianRatingList)
+        mainGame["stddev"] =mixStddev(voteCountList,stdRatingList)
+        #add line to dataframe
+    return pd.concat([rankedDF,mainGame], ignore_index=True)
+
+def weightedAverage(weights,averages):
+    sum = 0
+    for i in range(len(averages)):
+        sum = sum + averages[i]*weights[i]
+    return sum/(sum(weights))
+
+def mixStddev(sameplsizes,stddevs):
+    sum = 0
+    for i in range(len(stddevs)):
+        sum = sum + (stddevs[i]**2)/sameplsizes[i]
+    return math.sqrt(sum)
--- BGGData/main.py
+++ BGGData/main.py
@@ -10,17 +10,12 @@ import pandas as pd
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     dataFrame = pd.read_csv("RawData/Aggregated.csv")
-    # remove all subtyperanks nan so we can filer by them
-    dataFrame = dataFrame[dataFrame['subtype_rank'].notna()]
-    rankedDF = dataFrame.query("subtype_rank != 'Not Ranked'")
 
-    # Fill nan in average so we can filter 0s
-    rankedDF["bayesaverage"] = rankedDF.loc[:,"bayesaverage"].fillna(0)
-    rankedDF = rankedDF.query("bayesaverage != 0.0")
+    filteredDF = DataAnalysis.FilterData(dataFrame)
     #DataAnalysis.CountNonRanked(dataFrame)
-    reformated = reformatData.addMechanics(rankedDF)
-    print(reformated.head(10))
-    reformated.to_csv("RawData/ReformattedData.csv")
+    #reformated = reformatData.addMechanics(filteredDF)
+    #print(reformated.head(10))
+    filteredDF.to_csv("ReformattedData_filterTest.csv")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

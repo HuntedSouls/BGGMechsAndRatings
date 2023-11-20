cf853023115a4cdb39f0fcee6cbb938ff44f4b23
Joshua Kritz <joshuakritz2@gmail.com>
2023-11-20 09:33:52 +0000

    update reformatted data

--- BGGData/main.py
+++ BGGData/main.py
@@ -6,16 +6,19 @@ import reformatData
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     dataFrame = pd.read_csv("RawData/Aggregated.csv")
+    # remove all subtyperanks nan so we can filer by them
+    dataFrame = dataFrame[dataFrame['subtype_rank'].notna()]
+    rankedDF = dataFrame.query("subtype_rank != 'Not Ranked'")
+
+    # Fill nan in average so we can filter 0s
+    rankedDF["bayesaverage"] = rankedDF["bayesaverage"].fillna(0)
+    rankedDF = rankedDF.query("bayesaverage != 0.0")
     #DataAnalysis.CountNonRanked(dataFrame)
-    reformated = reformatData.addMechanics(dataFrame)
+    reformated = reformatData.addMechanics(rankedDF)
     print(reformated.head(10))
     reformated.to_csv("RawData/ReformatedData.csv")
 

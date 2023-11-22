1922391543536621c6247efe42835d42b96ba91f
Joshua Kritz <joshuakritz2@gmail.com>
2023-11-22 16:20:22 +0000

    mechanics filtered by grouping

--- BGGData/main.py
+++ BGGData/main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     # reformated = reformatData.addMechanics(dataFrame)
     #print(reformated.head(10))
     #filteredDF.to_csv("ReformattedData_filterTest.csv")
-    savepointDF = pd.read_csv("ReformattedData_final.csv")
+    savepointDF = pd.read_csv("ReformattedData_final_cleaned.csv")
 
     # gameIDList = savepointDF["gameID"].unique()
     # counter = 1000 # last save point!
@@ -25,8 +25,34 @@ if __name__ == '__main__':
     # mechanizedDF = reformatData.addMechanics(savepointDF)
     # mechanizedDF.to_csv("ReformattedData_final.csv")
 
-    #filter empty names
-    cleanedDF = savepointDF.dropna(subset=["name"])
-    cleanedDF.to_csv("ReformattedData_final_cleaned.csv")
+    # #filter empty names
+    # cleanedDF = savepointDF.dropna(subset=["name"])
+    # cleanedDF.to_csv("ReformattedData_final_cleaned.csv")
+
+    # grouping by mehcanics grouping
+    savepointDF = reformatData.JoinMehcanics("Area Movement",
+                                             ["Area Movement","Grid Movement","Point to Point Movement","Three Dimensional Movement"],savepointDF)
+    savepointDF = reformatData.JoinMehcanics("Auction",
+                                             ["Auction Compensation", "Auction/Bidding","Auction: Dexterity","Auction: Dutch","Auction: Dutch Priority",
+                                              "Auction: English","Auction: Fixed Placement","Auction: Multiple Lot","Auction: Once Around",
+                                              "Auction: Sealed Bid","Auction: Turn Order Until Pass",], savepointDF)
+    savepointDF = reformatData.JoinMehcanics("Deduction",
+                                             ["Deduction", "Induction"], savepointDF)
+    savepointDF = reformatData.JoinMehcanics("Grid Board",
+                                             ["Hexagon Grid", "Square Grid"], savepointDF)
+    savepointDF = reformatData.JoinMehcanics("Impulse",
+                                             ["Area-Impulse", "Impulse Movement"], savepointDF)
+    savepointDF = reformatData.JoinMehcanics("Pattern Movement",
+                                             ["Movement Template", "Pattern Movement"], savepointDF)
+    savepointDF = reformatData.JoinMehcanics("Predictive Bid",
+                                             ["Bids As Wagers", "Predictive Bid"], savepointDF)
+    savepointDF = reformatData.JoinMehcanics("Real-Time",
+                                             ["Real-Time", "Elapsed Real Time Ending"], savepointDF)
+    savepointDF = reformatData.JoinMehcanics("Turn Order",
+                                             ["Turn Order: Auction", "Turn Order: Claim Action", "Turn Order: Pass Order", "Turn Order: Progressive",
+                                              "Turn Order: Random", "Turn Order: Role Order","Turn Order: Stat-Based", "Turn Order: Time Track"], savepointDF)
+    savepointDF = reformatData.JoinMehcanics("Worker Placement",
+                                             ["Worker Placement", "Worker Placement with Dice Workers", "Worker Placement, Different Worker Types"], savepointDF)
+    savepointDF.to_csv("MechanicsFiltered.csv")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

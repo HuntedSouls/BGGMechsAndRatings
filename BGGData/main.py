8efe0c4f1fc8f342c31873d80569ece01ea8c32a
Joshua Kritz <joshuakritz2@gmail.com>
2023-11-23 15:31:01 +0000

    Auto stash before merge of "main" and "origin/main"

--- BGGData/main.py
+++ BGGData/main.py
@@ -2,6 +2,7 @@
 import APIGet
 import DataAnalysis
 import reformatData
+import matplotlib.pyplot as plt
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
@@ -14,7 +15,7 @@ if __name__ == '__main__':
     # reformated = reformatData.addMechanics(dataFrame)
     #print(reformated.head(10))
     #filteredDF.to_csv("ReformattedData_filterTest.csv")
-    savepointDF = pd.read_csv("ReformattedData_final_cleaned.csv")
+    # savepointDF = pd.read_csv("ReformattedData_final_cleaned.csv")
 
     # gameIDList = savepointDF["gameID"].unique()
     # counter = 1000 # last save point!
@@ -29,30 +30,44 @@ if __name__ == '__main__':
     # cleanedDF = savepointDF.dropna(subset=["name"])
     # cleanedDF.to_csv("ReformattedData_final_cleaned.csv")
 
-    # grouping by mehcanics grouping
-    savepointDF = reformatData.JoinMehcanics("Area Movement",
-                                             ["Area Movement","Grid Movement","Point to Point Movement","Three Dimensional Movement"],savepointDF)
-    savepointDF = reformatData.JoinMehcanics("Auction",
-                                             ["Auction Compensation", "Auction/Bidding","Auction: Dexterity","Auction: Dutch","Auction: Dutch Priority",
-                                              "Auction: English","Auction: Fixed Placement","Auction: Multiple Lot","Auction: Once Around",
-                                              "Auction: Sealed Bid","Auction: Turn Order Until Pass",], savepointDF)
-    savepointDF = reformatData.JoinMehcanics("Deduction",
-                                             ["Deduction", "Induction"], savepointDF)
-    savepointDF = reformatData.JoinMehcanics("Grid Board",
-                                             ["Hexagon Grid", "Square Grid"], savepointDF)
-    savepointDF = reformatData.JoinMehcanics("Impulse",
-                                             ["Area-Impulse", "Impulse Movement"], savepointDF)
-    savepointDF = reformatData.JoinMehcanics("Pattern Movement",
-                                             ["Movement Template", "Pattern Movement"], savepointDF)
-    savepointDF = reformatData.JoinMehcanics("Predictive Bid",
-                                             ["Bids As Wagers", "Predictive Bid"], savepointDF)
-    savepointDF = reformatData.JoinMehcanics("Real-Time",
-                                             ["Real-Time", "Elapsed Real Time Ending"], savepointDF)
-    savepointDF = reformatData.JoinMehcanics("Turn Order",
-                                             ["Turn Order: Auction", "Turn Order: Claim Action", "Turn Order: Pass Order", "Turn Order: Progressive",
-                                              "Turn Order: Random", "Turn Order: Role Order","Turn Order: Stat-Based", "Turn Order: Time Track"], savepointDF)
-    savepointDF = reformatData.JoinMehcanics("Worker Placement",
-                                             ["Worker Placement", "Worker Placement with Dice Workers", "Worker Placement, Different Worker Types"], savepointDF)
-    savepointDF.to_csv("MechanicsFiltered.csv")
+    # # grouping by mehcanics grouping
+    # savepointDF = reformatData.JoinMehcanics("Area Movement",
+    #                                          ["Area Movement","Grid Movement","Point to Point Movement","Three Dimensional Movement"],savepointDF)
+    # savepointDF = reformatData.JoinMehcanics("Auction",
+    #                                          ["Auction Compensation", "Auction/Bidding","Auction: Dexterity","Auction: Dutch","Auction: Dutch Priority",
+    #                                           "Auction: English","Auction: Fixed Placement","Auction: Multiple Lot","Auction: Once Around",
+    #                                           "Auction: Sealed Bid","Auction: Turn Order Until Pass",], savepointDF)
+    # savepointDF = reformatData.JoinMehcanics("Deduction",
+    #                                          ["Deduction", "Induction"], savepointDF)
+    # savepointDF = reformatData.JoinMehcanics("Grid Board",
+    #                                          ["Hexagon Grid", "Square Grid"], savepointDF)
+    # savepointDF = reformatData.JoinMehcanics("Impulse",
+    #                                          ["Area-Impulse", "Impulse Movement"], savepointDF)
+    # savepointDF = reformatData.JoinMehcanics("Pattern Movement",
+    #                                          ["Movement Template", "Pattern Movement"], savepointDF)
+    # savepointDF = reformatData.JoinMehcanics("Predictive Bid",
+    #                                          ["Bids As Wagers", "Predictive Bid"], savepointDF)
+    # savepointDF = reformatData.JoinMehcanics("Real-Time",
+    #                                          ["Real-Time", "Elapsed Real Time Ending"], savepointDF)
+    # savepointDF = reformatData.JoinMehcanics("Turn Order",
+    #                                          ["Turn Order: Auction", "Turn Order: Claim Action", "Turn Order: Pass Order", "Turn Order: Progressive",
+    #                                           "Turn Order: Random", "Turn Order: Role Order","Turn Order: Stat-Based", "Turn Order: Time Track"], savepointDF)
+    # savepointDF = reformatData.JoinMehcanics("Worker Placement",
+    #                                          ["Worker Placement", "Worker Placement with Dice Workers", "Worker Placement, Different Worker Types"], savepointDF)
+    # savepointDF.to_csv("MechanicsFiltered.csv")
 
+    ### data analysis part
+    # unfiltered graphics
+    # dataFrame = pd.read_csv("RawData/ReformatedData_unfiltered.csv")
+    # DataAnalysis.GnerateHistograms(dataFrame)
+    # plt.show()
+    # DataAnalysis.MechanicsGraphics(dataFrame,26)
+    # plt.show()
+
+    # filtered graphics
+    dataFrame = pd.read_csv("ReformattedData_final_cleaned.csv")
+    # DataAnalysis.GnerateHistograms(dataFrame)
+    # plt.show()
+    DataAnalysis.MechanicsGraphics(dataFrame,29)
+    plt.show()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- BGGData/reformatData.py
+++ BGGData/reformatData.py
@@ -1,3 +1,4 @@
+import math
 import pandas as pd
 import numpy as np
 def repeatUniqueGameIDS(df):
@@ -72,3 +73,123 @@ def addMechanics(df):
         print("done on mech ",mechanic)
     mechanicDF = pd.DataFrame(data)
     return reformated.join(mechanicDF)
+
+def JoinMehcanics(newName, listOfNames, dataframe):
+    newOperator = pandas.Series([0]*len(dataframe.index))
+    for name in listOfNames:
+        newOperator = newOperator | dataframe[name]
+        dataframe=dataframe.drop(columns=[name])
+    dataframe[newName] = newOperator
+    return dataframe
+
+def FilterRankData(dataFrame):
+    #remove all subtyperanks nan so we can filer by them
+    #filterDF = dataFrame[dataFrame['subtype_rank'].notna()]
+    blacklistDF = dataFrame.query("subtype_rank != 'Not Ranked'")
+
+    #Fill nan in average so we can filter 0s
+    blacklistDF["bayesaverage"] =blacklistDF["bayesaverage"].fillna(0)
+    blacklistDF = blacklistDF.query("bayesaverage != 0.0")
+
+    whiteListedIDS=blacklistDF["gameID"].unique()
+    filteredDF = dataFrame.loc[dataFrame['gameID'].isin(whiteListedIDS)]
+    # for gameID in blacklistedGameIDs:
+    #     filteredDF = filteredDF.loc[not filteredDF['gameID'].isin(gameID)]
+    return filteredDF
+
+def FilterImplementationData(filteredDF,gameIDList,counter):
+    implList = filteredDF.groupby("gameID")["implementation"].apply(list).reset_index(name='impls')
+    #counter = 0
+    # gameIDList=filteredDF["gameID"].unique()
+    # filter through implementations
+    solvedGameID = []
+    for gameID in gameIDList:
+        implementations = implList.loc[implList['gameID'] == gameIDList[counter]]
+        if (not solvedGameID.__contains__(gameID)) and (not implementations.empty):
+            cleaned = [x for x in implementations.iloc[0]["impls"] if str(x) != 'nan']
+            listOfGameIDS = [gameIDList[counter]]
+            solvedGameID.append(gameIDList[counter])
+            for name in cleaned:
+                foundGames = filteredDF.loc[filteredDF['name'] == name]
+                if not foundGames.empty:
+                    newID = foundGames.iloc[0]["gameID"]
+                    listOfGameIDS.append(newID)
+                    solvedGameID.append(newID)
+            filteredDF = HandleReimplementations(filteredDF, listOfGameIDS)
+        # else:
+        #     print("already done this! ",gameID)
+        if counter >= len(gameIDList)-1:
+            filteredDF.to_csv("RawData/ReformattedData_filterTest_" + str(counter) + "_v2.csv")
+            break
+        counter = counter+1
+        if counter%1000==0:
+            filteredDF.to_csv("RawData/ReformattedData_filterTest_"+str(counter)+"_v2.csv")
+            print("created save point: ", counter)
+
+        #print("finished gameID : ", gameID)
+
+    return filteredDF
+
+def HandleReimplementations(rankedDF, listOfGameIDS):
+    mechList = rankedDF.groupby("gameID")["mechanic"].apply(list).reset_index(name='mechs')
+    thisMechs ={}
+    for gameID in listOfGameIDS:
+        mechanics = mechList.loc[mechList['gameID'] == gameID]
+        cleaned = [x for x in mechanics if str(x) != 'nan']
+        thisMechs[gameID] = cleaned
+
+    equal=[]
+    for gameID in listOfGameIDS:
+        if gameID == listOfGameIDS[0]:
+            continue
+        if thisMechs[gameID].sort() == thisMechs[listOfGameIDS[0]].sort():
+            equal.append(gameID)
+    #do some magic if they found equal games
+    mainGame = pd.Series()
+    if len(equal)>0:
+        # print("IVE found some equals! they are:", equal)
+        avgRatingList =[]
+        baeysianRatingList =[]
+        stdRatingList =[]
+        voteCountList=[]
+        yearList=[]
+        equal.append(listOfGameIDS[0])
+        for gameID in equal:
+            selected = rankedDF.loc[rankedDF['gameID'] == gameID]
+            avgRatingList.append(selected.iloc[0]["average"])
+            baeysianRatingList.append(selected.iloc[0]["bayesaverage"])
+            stdRatingList.append(selected.iloc[0]["stddev"])
+            voteCountList.append(selected.iloc[0]["usersrated"])
+            yearList.append(selected.iloc[0]["yearpublished"])
+        #define the older year to be the entry and delete all "tied" games
+        olderYear = min(yearList)
+        olderGameID = equal[yearList.index(olderYear)]
+        mainGame = rankedDF.loc[rankedDF['gameID'] == olderGameID]
+        mainGame = mainGame.iloc[0]
+        #Also remove the original
+        for gameID in equal:
+            rankedDF = rankedDF[~((rankedDF["gameID"] == gameID) & (~rankedDF["name"].isna()))]
+        #and do some math see combine averages and stdeviation
+        mainGame["average"] = weightedAverage(voteCountList,avgRatingList)
+        mainGame["bayesaverage"] = weightedAverage(voteCountList, baeysianRatingList)
+        mainGame["stddev"] =mixStddev(voteCountList,stdRatingList)
+        mainGame["usersrated"] =sum(voteCountList)
+        #add line to dataframe
+    if not mainGame.empty:
+        rankedDF = rankedDF[~((rankedDF["gameID"] == olderGameID) & (~rankedDF["name"].isna()))]
+        newDataFrame=pd.concat([rankedDF,mainGame.to_frame().T])
+    else:
+        newDataFrame = rankedDF
+    return newDataFrame
+
+def weightedAverage(weights,averages):
+    total = 0
+    for i in range(len(averages)):
+        total = total + averages[i]*weights[i]
+    return total/(sum(weights))
+
+def mixStddev(sameplsizes,stddevs):
+    total = 0
+    for i in range(len(stddevs)):
+        total = total + (stddevs[i]**2)/sameplsizes[i]
+    return math.sqrt(total)
